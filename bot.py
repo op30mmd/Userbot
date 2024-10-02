170dfad7d42f2fa385c147ee49e1cc38ef640b99
op30mmd <152014555+op30mmd@users.noreply.github.com>
2024-10-02 13:59:52 +0000

    به تو چه

--- bot.py
+++ bot.py
@@ -19,12 +19,26 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
+COMMAND_PREFIX = "."
 
 client = TelegramClient(StringSession(os.environ.get("SESSION")), os.environ.get("API_ID"), os.environ.get("API_HASH"))
 
 @client.on(events.NewMessage(pattern='/hi'))
 async def get(event):
-    await event.reply('hi')
+    r_txt = event.raw_text.strip()
+    sender = await event.get_sender()
+    org_id = sender.id
+    command_parts = shlex.split(r_txt)
+    command_name = command_parts[0][len(COMMAND_PREFIX):].lower()
+    owner = 1630778333
+    if org_id != owner:
+        return
+    if not r_txt.startswith(COMMAND_PREFIX):
+        return  # Not a command
+    if command_name == 'echo':
+        if len(command_parts) < 2:
+            await event.reply('متن کیریتو وارد کن جلوی .echo')
+        await event.reply(command_parts[1])
 
 client.start()
 client.run_until_disconnected()
