1a7b2088357f77dfd9fb5a3809a74334e41845aa
mmty <pdrsgslwaty@gmail.com>
2024-10-03 10:23:44 +0000

    most features added

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ from telethon.tl.types import Message
 from telethon.tl.types import UpdateBotNewBusinessMessage
 from telethon.tl.functions import InvokeWithBusinessConnectionRequest
 from telethon.tl.functions.stories import GetStoriesByIDRequest
+from telethon.tl.custom import ParticipantPermission
 import shlex
 import subprocess
 import tempfile
@@ -82,5 +83,102 @@ async def userbot(event):
         await client.edit_message(event.chat_id, event.id, f'Downloading file: `{f_name}`')
         await client.download_media(message)
         await event.reply('Success')
+
+    elif command_name == 'del':
+        if not event.is_reply:
+            await event.reply('Usage: .del <reply>')
+            return
+        message = await event.get_reply_message()
+        await client.delete_messages(event.chat_id, [message.id])
+        await client.edit_message(event.chat_id, event.id, 'Success')
+
+    elif command_name == 'promote':
+        if not event.is_reply:
+            await event.reply('Usage: .promote <reply>')
+            return
+        message = await event.get_reply_message()
+        await client.edit_message(event.chat_id, event.id, 'Promoting user...')
+        await client.edit_admin(
+            event.chat_id,
+            message.sender_id,
+            change_info=True,
+                post_messages=True,
+                edit_messages=True,
+                delete_messages=True,
+                ban_users=True,
+                invite_users=True,
+                pin_messages=True,
+                add_admins=False  # Adjust as needed
+        )
+        await client.edit_message(event.chat_id, event.id, 'Success')
+
+    elif command_name == 'demote':
+        if not event.is_reply:
+            await event.reply('Usage: .demote <reply>')
+            return
+        message = await event.get_reply_message()
+        await client.edit_message(event.chat_id, event.id, 'Demoting user...')
+        await client.edit_admin(
+            event.chat_id,
+            message.sender_id,
+            is_admin=False
+        )
+        await client.edit_message(event.chat_id, event.id, 'Success')
+
+    elif command_name == 'kick':
+        if not event.is_reply:
+            await event.reply('Usage: .kick <reply>')
+            return
+        message = await event.get_reply_message()
+        await client.edit_message(event.chat_id, event.id, 'Kicking user...')
+        await client.kick_participant(event.chat_id, message.sender_id)
+        await client.edit_message(event.chat_id, event.id, 'Success')
+
+    elif command_name == 'ban':
+        if not event.is_reply:
+            await event.reply('Usage: .ban <reply>')
+            return
+        message = await event.get_reply_message()
+        await client.edit_message(event.chat_id, event.id, 'Banning user...')
+        await client.edit_permissions(event.chat_id, message.sender_id, view_messages=False)
+        await client.edit_message(event.chat_id, event.id, 'Success')
+
+    elif command_name == 'unban':
+        if not event.is_reply:
+            await event.reply('Usage: .unban <reply>')
+            return
+        message = await event.get_reply_message()
+        await client.edit_message(event.chat_id, event.id, 'Unbanning user...')
+        await client.edit_permissions(event.chat_id, message.sender_id)
+        await client.edit_message(event.chat_id, event.id, 'Success')
+
+    elif command_name == 'id':
+        if not event.is_reply:
+            await event.reply('Usage: .id <reply>')
+            return
+        message = await event.get_reply_message()
+        gpeer = await client.get_entity(message.sender_id)
+        f_name = utils.get_display_name(gpeer)
+        await client.edit_message(event.chat_id, event.id, f"{f_name}'s ID: `{message.sender_id}`")
+
+    elif command_name == 'pin':
+        if not event.is_reply:
+            await event.reply('Usage: .pin <reply>')
+            return
+        message = await event.get_reply_message()
+        await client.edit_message(event.chat_id, event.id, 'Pinning message...')
+        await message.pin(notify=False)
+        await client.edit_message(event.chat_id, event.id, 'Success')
+
+    elif command_name == 'unpin':
+        if not event.is_reply:
+            await event.reply('Usage: .unpin <reply>')
+            return
+        message = await event.get_reply_message()
+        await client.edit_message(event.chat_id, event.id, 'Unpinning message...')
+        await message.unpin()
+        await client.edit_message(event.chat_id, event.id, 'Success')
+
+    
 client.start()
 client.run_until_disconnected()
