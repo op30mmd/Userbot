d542c3336275f00019427bd73f53ca1a7c37df4d
op30mmd <152014555+op30mmd@users.noreply.github.com>
2024-10-10 17:34:22 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -340,7 +340,7 @@ async def userbot(event):
         
         try:
             msg = await event.get_reply_message()
-            await client.edit_message(event.chat_id, event.id, f"**Message info**\n\nMessage ID: `{msg.id}`\nMentioned?: `{msg.mentioned}`\nMedia Unread?: `{msg.media_unread}`\nPost?: `{msg.post}`\nScheduled?: `{msg.from_scheduled}`\nLegacy?: `{msg.legacy}`\nPinned?: `{msg.pinned}`\nForwardable?: `{msg.noforwards}`\nOffline?: `{msg.offline}`\nUserID: `{event.}`\nPeerID: `{msg.sender_id}`\nViews: `{msg.views}`\nInline BotID: `{msg.via_bot_id}`\nForwards: `{msg.forwards}`\nReplies: `{msg.replies.replies}`\nEdit Date: `{msg.edit_date}`\nPost Author: `{msg.post_author}`\nChannel ID: `{msg.from_id.channel_id}`")
+            await client.edit_message(event.chat_id, event.id, f"**Message info**\n\nMessage ID: `{msg.id}`\nMentioned?: `{msg.mentioned}`\nMedia Unread?: `{msg.media_unread}`\nPost?: `{msg.post}`\nScheduled?: `{msg.from_scheduled}`\nLegacy?: `{msg.legacy}`\nPinned?: `{msg.pinned}`\nForwardable?: `{msg.noforwards}`\nOffline?: `{msg.offline}`\nUserID: `{msg.sender_id}`\nPeerID: `{msg.peer_id.channel_id}`\nViews: `{msg.views}`\nInline BotID: `{msg.via_bot_id}`\nForwards: `{msg.forwards}`\nReplies: `{msg.replies.replies}`\nEdit Date: `{msg.edit_date}`\nPost Author: `{msg.post_author}`\nChannel ID: `{msg.from_id.channel_id}`")
         except Exception as e:
             await client.edit_message(event.chat_id, event.id, f"Error: {e}")
     
